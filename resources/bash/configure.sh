3cc724192e78d2a57dfa7f79b53d81c71784c465
Alex Bleasdale <ableasdale@marklogic.com>
2012-03-08 10:50:32 +0000

    Shell script for full end-to-end automation

---
+++ resources/bash/configure.sh
@@ -0,0 +1,9 @@
+HOSTS=(ip-10-218-43-190 ip-10-102-37-236 ip-10-62-167-204 ip-10-62-215-29 ip-10-60-189-202 ip-10-218-79-223 ip-10-102-3-196)
+BASE_FOLDER=/mnt
+
+i=0; for h in ${HOSTS[@]}; do
+        scp -r rebalance root@$h:$BASE_FOLDER
+        scp $h.ec2.internal/*.properties root@$h:$BASE_FOLDER/rebalance
+done
+
+i=0; (for h in ${HOSTS[@]}; do echo screen -t \"screen $i\" $i bash -c '"'ssh $h "'"cd $BASE_FOLDER/rebalance \&\& ./run.sh\; echo hit return\; read"'" '"'; i=$((i = i + 1)); done; echo screen $i) > screen-commands
