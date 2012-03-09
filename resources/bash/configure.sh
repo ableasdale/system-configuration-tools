a2dd371d7ddcdbee5dafa930fb0aff578f5c6cfb
Alex Bleasdale <ableasdale@marklogic.com>
2012-03-09 09:47:34 +0000

    Added preflight screen check to avoid having multiple screen instances active

--- resources/bash/configure.sh
+++ resources/bash/configure.sh
@@ -1,6 +1,12 @@
 HOSTS=(ip-10-218-43-190 ip-10-102-37-236 ip-10-62-167-204 ip-10-62-215-29 ip-10-60-189-202 ip-10-218-79-223 ip-10-102-3-196)
 BASE_FOLDER=/mnt
 
+# preflight
+if [ "$TERM" == "screen" ]; then
+    echo exit screen before running this script!
+    exit 1
+fi
+
 i=0; for h in ${HOSTS[@]}; do
         scp -r rebalance root@$h:$BASE_FOLDER
         scp /tmp/$h*.properties root@$h:$BASE_FOLDER/rebalance
