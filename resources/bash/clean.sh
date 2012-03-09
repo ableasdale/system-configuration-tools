79a99ab9412c450fb16a23e6f16e6da8ce0adf4e
Alex Bleasdale <ableasdale@marklogic.com>
2012-03-09 13:21:59 +0000

    improved clean operation across cluster

--- resources/bash/clean.sh
+++ resources/bash/clean.sh
@@ -4,6 +4,5 @@ HOSTS=$2
 i=0; for h in ${HOSTS[@]}; do
 	echo removing files for $h ...
 	rm /tmp/$h*.properties
-        #scp -r rebalance root@$h:$BASE_FOLDER
-        # scp /tmp/$h*.properties root@$h:$BASE_FOLDER/rebalance
+	ssh -A root@localhost "rm -rf $BASE_FOLDER/rebalance"
 done
